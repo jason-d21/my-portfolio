64287f75c8a05a1fe07fe24963cb21766dac1ebd
jason-d21 <47430250+jason-d21@users.noreply.github.com>
2023-04-03 23:51:14 -0700

    About page basics

--- src/constants/index.js
+++ src/constants/index.js
@@ -42,14 +42,6 @@ import {
   
   const services = [
     {
-      title: "Web Developer",
-      icon: web,
-    },
-    {
-      title: "React Native Developer",
-      icon: mobile,
-    },
-    {
       title: "Backend Developer",
       icon: backend,
     },
